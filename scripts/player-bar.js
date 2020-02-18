5be68a56f87ac314e16ce2c72bfdf341902236e2
Sabrina Jackson <sabrina@wolfchild.com>
2020-02-18 11:22:02 -0800

    Assignment_11_Bloc_Jams_jQuery: Revision Completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -29,8 +29,7 @@
         const percent = (currentTime / duration) * 100;
         $('#time-control .current-time').text( currentTime);
         $('#time-control input').val(percent);
-    }, 1000);
-
+    }, 1000); 
 
 
     $('button#previous').on('click', function() {
